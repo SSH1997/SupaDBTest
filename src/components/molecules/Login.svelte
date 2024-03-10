86f877437c8b69dbd509e1ebb77ad51de52b626b
SSH1997 [akasiassh] <akasiassh@naver.com>
2024-03-10 18:20:02 +0900

    [FEATURE] Add Css To Login

--- src/components/molecules/Login.svelte
+++ src/components/molecules/Login.svelte
@@ -14,20 +14,26 @@
 
     const initPassCode = () => {
         totalPassCode = "";
-        totalPassCodeDigit = "______";
+        totalPassCodeDigit = "_ _ _ _ _ _";
     }
 
     const insertPassCode = (passCode: string) => {
         totalPassCode += passCode;
 
-        var splitedDigits = totalPassCodeDigit.split("");
+        var splitedDigits = totalPassCodeDigit.split(" ");
         splitedDigits.unshift("*");
         splitedDigits.pop();
-        totalPassCodeDigit = splitedDigits.join("");
+
+        if (splitedDigits.every(it => it == "*")) {
+            submitPassCode();
+        }
+        else {
+            totalPassCodeDigit = splitedDigits.join(" ");
+        }
     }
 
     const submitPassCode = () => {
-        if (totalPassCode == "▲▲▲▲▲▲") {
+        if (totalPassCode == "AABCCD") {
             setSectionContent(SectionContent.Calendar);
         } else {
             alert("비밀번호가 틀렸습니다!");
@@ -37,20 +43,81 @@
 </script>
 
 <div>
-    <div>
-        {totalPassCodeDigit}
-    </div>
+    <img class="digivice_background" src="https://ssh1997.github.io/SupaDBTest/Digivice.webp" alt="" />
 
-    <div>
-        <Button buttonText="▲" onClick={() => insertPassCode("▲")}></Button>
+    <div class="passcode_zone">
+        <div class="left_passcode_div">
+            <Button buttonText="" onClick={() => insertPassCode("A")}></Button>
+            <Button buttonText="" onClick={() => insertPassCode("B")}></Button>
+        </div>
+        <div class="middle_passcode_div">
+            <div class="passcode_digit_background" />
+            <div class="passcode_digit">
+                {totalPassCodeDigit}
+            </div>
+        </div>
+        <div class="right_passcode_div">
+            <Button buttonText="" onClick={() => insertPassCode("C")}></Button>
+            <Button buttonText="" onClick={() => insertPassCode("D")}></Button>
+        </div>
     </div>
-    <div style="display: flex;">
-        <Button buttonText="◀︎" onClick={() => insertPassCode("◀︎")}></Button>
-        <Button buttonText="▼" onClick={() => insertPassCode("▼")}></Button>
-        <Button buttonText="▶︎" onClick={() => insertPassCode("▶︎")}></Button>
-    </div>
-    <Button buttonText="Submit" onClick={submitPassCode}></Button>
 </div>
 
 <style>
+    .digivice_background {
+        position: absolute;
+        background-color: aqua;
+        width: 100vw;
+        top: 15vh;
+    }
+
+    .passcode_zone {
+        position: absolute;
+        top: calc(15vh + 8vw);
+        
+        width: 100vw;
+        height: 59vw;
+
+        display: flex;
+    }
+
+    .left_passcode_div {
+        display: flex;
+        flex-direction: column;
+
+        width: 30%;
+    }
+
+    .middle_passcode_div {
+        width: 40%;
+        height: 100%;
+    }
+
+    .passcode_digit_background {
+        position: absolute;
+        top: 45%;
+        left: 35%;
+        width: 30vw;
+        height: 30vw;
+
+        border-radius: 10%;
+
+        background-color: rgb(97, 104, 88);
+    }
+
+    .passcode_digit {
+        position: absolute;
+        top: 65%;
+        left: 37%;
+        font-size: 6vw;
+    }
+
+    .right_passcode_div {
+        display: flex;
+        flex-direction: column;
+
+        width: 30%;
+        height: 59%;
+        padding-top: 24%;
+    }
 </style>
